fca19862f462a5fa1d54ab2a2287241d61d3d1eb
mayankpcp7 <jariwalmayank123@gmail.com>
2024-08-21 12:34:58 +0530

    first commit

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { IMAGE_DATA, TAB_IMAGES } from "../utils/Helper";
+
+const settings = {
+  arrows: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  fade: true,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true,
+      },
+    },
+  ],
+};
+
+const ImageSlider = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const sliderRef = useRef(null);
+
+  // Handle slider changes
+  const handleAfterChange = (current) => {
+    setActiveTab(current);
+  };
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+    if (sliderRef.current) {
+      sliderRef.current.slickGoTo(index);
+    }
+  };
+
+  return (
+    <div className="container mx-auto px-4">
+      <h2 className="text-center mb-10 font-semibold text-4xl text-black">
+        TABS SLIDER
+      </h2>
+
+      <Slider
+        ref={sliderRef}
+        className=" mx-auto"
+        {...settings}
+        initialSlide={activeTab}
+        afterChange={handleAfterChange}
+      >
+        {IMAGE_DATA.map((image, index) => (
+          <div className="sizes-[100vw] mx-auto" key={image.id}>
+            <img
+              className="w-full h-[200px]  md:h-[500px] object-cover"
+              src={image.src}
+              alt={`slide ${image.id}`}
+            />
+          </div>
+        ))}
+      </Slider>
+
+      {/* Tabs */}
+      <div className="flex flex-wrap gap-4 md:gap-6 justify-center items-center mt-10 md:mt-6">
+        {TAB_IMAGES.map((image, index) => (
+          <img
+            key={index}
+            src={image.imgSrc}
+            className={`lg:w-[200px] w-[100px] h-[70px] lg:h-[100px] object-cover cursor-pointer ${
+              activeTab === index ? "border-2 border-blue-500" : ""
+            }`}
+            onClick={() => handleTabClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
---
+++ src/utils/Helper.js
@@ -0,0 +1,25 @@
+// utils/Helper.js
+
+import slide1 from "../assets/images/webp/slide-1.webp";
+import slide2 from "../assets/images/webp/slide-5.webp";
+import slide3 from "../assets/images/webp/slide-3.webp";
+import slide4 from "../assets/images/webp/slide-4.webp";
+import slide5 from "../assets/images/webp/slide-5.webp";
+
+
+
+export const IMAGE_DATA = [
+  { id: 1, src: slide1 },
+  { id: 2, src: slide2 },
+  { id: 3, src: slide3 },
+  { id: 4, src: slide4 },
+  { id: 5, src: slide5 },
+];
+
+export const TAB_IMAGES = [
+  { imgSrc: slide1 },
+  { imgSrc: slide2 },
+  { imgSrc: slide3 },
+  { imgSrc: slide4 },
+  { imgSrc: slide5 },
+];
